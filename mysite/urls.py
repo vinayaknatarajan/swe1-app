568a4f009ae21e45ede23aa0c1e8c34d9a7d3f91
vinayaknatarajan1 <nvinayak777@gmail.com>
2020-10-19 19:05:04 -0400

    added travis yml file

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,4 @@ from django.urls import path
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
-]
-
+urlpatterns = [path("admin/", admin.site.urls), path("polls/", include("polls.urls"))]
